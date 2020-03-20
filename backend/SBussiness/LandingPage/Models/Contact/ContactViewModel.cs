70a3194db765451c9d1b4d50b00b217fbb2f5625
hoangdev1808 <hoangdev1808@gmail.com>
2020-03-20 13:12:09 +0700

    FIX ADDRESS

--- backend/SBussiness/LandingPage/Models/Contact/ContactViewModel.cs
+++ backend/SBussiness/LandingPage/Models/Contact/ContactViewModel.cs
@@ -15,8 +15,7 @@ namespace MainProject.SBussiness.LandingPage.Models.Contact
             Address = "<span>Địa chỉ:</span><p> Tòa nhà H3,384 Hoàng Diệu,Quận 4,TP.Hồ Chí Minh </p> ",
             Email = "<span>Email:</span><p> info@antingroup.vn </p> ",
             Fax = "<span>Fax:</span><p> 028. 38266766 </p> ",
-            Lat = Convert.ToDecimal(10.1251),
-            Lng = Convert.ToDecimal(11.125),
+            AddressMap = "<div id=\"map\"></div>",
             Phone = "<div class=\"icon\"><img src=\"/content/resources/assets/icons/phone.png\" alt=\"alt\"></div><a href=\"tel:02838266766\">028. 38266766</a>"
         };
         public Category Category { get; set; } = new Category
